3e723265cc176ea3563c62057d0bcacf6af0c307
Holly Heredia <hollyheredia@gmail.com>
2021-04-29 19:57:21 -0700

    First commit. CSV found on the web of population info along with a pull of census data.

---
+++ Data/api_key.py
@@ -0,0 +1,2 @@
+g_key = "AIzaSyCemi2RffQlQfXyqGZnAPByHN5dxUHfcjQ"
+census_key = "5b0814a0a6d1ad3fb0dd79797711accb5f5f98a9"
---
+++ api_key.py
@@ -0,0 +1,3 @@
+ API Key
+ g_key = "AIzaSyCemi2RffQlQfXyqGZnAPByHN5dxUHfcjQ"
+ census_key = "5b0814a0a6d1ad3fb0dd79797711accb5f5f98a9"
