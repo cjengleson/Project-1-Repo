b963015b6935f2827f8efabeb68c955d772937a3
Holly Heredia <hollyheredia@gmail.com>
2021-05-05 16:56:03 -0700

    More attempts at pulling Census API. Found CSV files for download on the internet instead.

---
+++ Old Notebooks/api_key.py
@@ -0,0 +1,3 @@
+ API Key
+ g_key = "AIzaSyCemi2RffQlQfXyqGZnAPByHN5dxUHfcjQ"
+ census_key = "b237421d8e633037d342ceed1ad93065dfc3ce37"
---
+++ Old Notebooks/config.py
@@ -0,0 +1,5 @@
+# Census API Key
+api_key = "b237421d8e633037d342ceed1ad93065dfc3ce37"
+
+# Google API Key
+g_key = "AIzaSyCemi2RffQlQfXyqGZnAPByHN5dxUHfcjQ"
